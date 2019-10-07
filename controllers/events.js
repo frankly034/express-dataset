3180d2b8e380aedaf24fd53b9fd15e8c58722bcb
frankly034 <franklynugege@gmail.com>
2019-10-07 03:58:54 +0100

    ft(events): add events endpoints
    - add events controller
    - add events middleware
    - add events routes

--- controllers/events.js
+++ controllers/events.js
@@ -1,20 +1,60 @@
+const { Event } = require('../models');
+const { handleResponse } = require('../helpers/utils');
 
-var getAllEvents = () => {
-
+var getAllEvents = async (req, res) => {
+	let events;
+	try {
+		events = await Event.findAll({
+			attributes: ['id', 'type', 'created_at'],
+			include: [{ all: true }],
+			order: [ ['id', 'ASC'] ]
+		})
+	} catch(e){
+		return handleResponse(res);
+	}
+	return handleResponse(res, 200, events);
 };
 
-var addEvent = () => {
-
+var addEvent = async (req, res) => {
+	let event;
+	try{
+		event = await Event.create(req.body, {
+			attributes: ['id', 'type', 'created_at'],
+			include: [{ all: true }]
+		});
+	} catch(e) {
+		return handleResponse(res);
+	}
+	return handleResponse(res, 201, event);
 };
 
 
-var getByActor = () => {
-
+var getByActor = async (req, res) => {
+	let events;
+	const { actorId } = req.params;
+	try {
+		events = await Event.findAll({
+			where: { actorId },
+			attributes: ['id', 'type', 'created_at'],
+			include: [{ all: true }],
+			order: [ ['id', 'ASC'] ]
+		})
+	} catch(e){
+		return handleResponse(res, 404);
+	}
+	return events.length ?
+		handleResponse(res, 200, events) :
+		handleResponse(res, 404);
 };
 
 
-var eraseEvents = () => {
-
+var eraseEvents = async (req, res) => {
+	try {
+		await Event.destroy({ where: {} });
+	} catch(e){
+		return handleResponse(res);
+	}
+	return handleResponse(res, 200);
 };
 
 module.exports = {
--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,11 @@
 var express = require('express');
+const { addEvent, getAllEvents, getByActor } = require('../controllers/events');
+const { validateNonexistentEvent } = require('../middlewares/event');
 var router = express.Router();
 
 // Routes related to event
-
+router.post('/', validateNonexistentEvent, addEvent);
+router.get('/', getAllEvents);
+router.get('/actors/:actorId', getByActor);
 
 module.exports = router;
